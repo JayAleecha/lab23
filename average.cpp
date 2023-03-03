6f50797b24f73c33247419653b3bd8c9a80b21db
JayAleecha <aleecha.2546@gmail.com>
2023-03-03 15:27:19 +0700

    average.cpp comp.

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+using namespace std;
+int main(int A , char *B[]){
+    double avg=0;
+    if(A == 1){
+        cout << "Please input numbers to find average.\n";
+    }else{
+        for(int i=1 ; i<A ; i++){
+            avg+=atoi(B[i]);
+        }
+        avg = avg/(A-1);
+    cout << "---------------------------------\n";
+    cout << "Average of " << A-1 << " numbers = " << avg << endl;
+    cout << "---------------------------------\n";    
+    }
+            
+    
+}
